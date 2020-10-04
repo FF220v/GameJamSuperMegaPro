9cb8033f18360c9766368a18b5bd7cc9e693b248
Bagmutov <bagmutov94@mail.ru>
2020-10-04 22:41:17 +0300

    dialog + floors

---
+++ New Unity Project/Assets/Floors.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floors : MonoBehaviour
+{
+    Transform pl;
+    public int xdist=4,zdist=4, floorH=2;
+    public GameObject[] floors;
+    int inside=100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pl=GameObject.FindObjectOfType<PlControl>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int newins=inside;
+        Vector3 dist=pl.position-transform.position;
+        if(Mathf.Abs(dist.x)<xdist && Mathf.Abs(dist.z)<zdist){
+            newins=(int)Mathf.Floor(pl.position.y/floorH);
+            // Debug.Log(inside+" "+dist);
+        }else{
+            newins=100;
+        }
+        if(inside!=newins){
+            inside=newins;
+            for(int i=0;i<floors.Length;i++){
+                    DisableAllRenderers(floors[i],i<inside);
+                }
+        }
+    }
+    void DisableAllRenderers(GameObject obj, bool val)
+    {
+        Renderer[] allRenderers;
+        allRenderers = obj.GetComponentsInChildren< Renderer >();
+        foreach(var re in allRenderers ){ re.enabled = val; }
+    }
+}
---
+++ New Unity Project/Assets/Static.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Static
+{
+    
+}
+
+public class Dialog{
+    public int state=0;
+    public virtual DLine getNextLine(int val=0){
+        switch(state){
+            case 0: break;
+        }
+        return null;
+    }
+}
+
+public class DLine{
+    public string str;
+    public int code;
+    public DLine(int codee, string strr)
+    {
+       str=strr;
+       code=codee;
+    }
+
+}
