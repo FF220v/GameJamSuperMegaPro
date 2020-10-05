97dc7f44e4c2cf7a51608a2529d3ce31a30b5a1d
Bagmutov <bagmutov94@mail.ru>
2020-10-05 22:47:20 +0300

    Sound

---
+++ New Unity Project/Assets/DiscSound.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiscSound : MonoBehaviour
+{
+    public AudioClip backHome;
+    public AudioClip backTower;
+    public AudioClip backBar;
+    public AudioClip backDream;
+    public AudioSource src;
+    public float ang1,ang2,ang3,ang4,ang5;
+    Transform tr;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!src.isPlaying){
+            float ay=transform.rotation.eulerAngles.y;
+            Debug.Log(" ay "+ay);
+            if(ay<ang1||ay>ang5){
+                src.PlayOneShot(backHome);
+            }else if(ay<ang2){
+                src.PlayOneShot(backTower);
+            }else if(ay<ang3){
+                src.PlayOneShot(backBar);
+            }else if(ay<ang4){
+                src.PlayOneShot(backDream);
+            }
+        }
+    }
+}
